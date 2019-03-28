6b90f986dace477e5531b19192e9e054f988c3f4
Rodrigo Romero <rodrigo.lima.ext@dasa.com.br>
2019-03-28 11:40:21 -0300

    ajustes

--- PrinterGliese/src/main/java/br/com/dasa/services/DadosImpressaoService.java
+++ PrinterGliese/src/main/java/br/com/dasa/services/DadosImpressaoService.java
@@ -8,13 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 import org.springframework.util.StringUtils;
 
 import br.com.dasa.consumers.ConsumerMQ;
-=======
-
->>>>>>> 99dee9c8fae28b89d35ac48b9065ef2f2918c2d4
 import br.com.dasa.dtos.ImpressoraDTO;
 import br.com.dasa.helpers.FileHelper;
 import br.com.dasa.helpers.SOHelper;
@@ -34,23 +30,19 @@ public class DadosImpressaoService {
 	@Autowired
 	private SOHelper soHelper; 
 	@Autowired
-<<<<<<< HEAD
 	private PrinterCoreService printerCoreService;
 	@Autowired
 	private ConsumerMQ consumerMQ; 
-=======
-	private PrinterCoreService printerCoreService; 
->>>>>>> 99dee9c8fae28b89d35ac48b9065ef2f2918c2d4
+
 	
 	public void salvarDadosImpressao(ImpressoraDTO impressoraDTO, EmpresaJson empresaJson, UnidadeJson unidadeJson) {
 		try {
 			Properties props = fileHelper.getProperties(urlPropertiesImpressao);
 			fileHelper.salvarProperties(urlPropertiesImpressao, props, getMapaProperties(empresaJson, unidadeJson));
 			salvarAlteracoesNoPrinterCore(impressoraDTO, empresaJson, unidadeJson);
-<<<<<<< HEAD
+
 			consumerMQ.consome();
-=======
->>>>>>> 99dee9c8fae28b89d35ac48b9065ef2f2918c2d4
+
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
@@ -58,11 +50,9 @@ public class DadosImpressaoService {
 
 	private void salvarAlteracoesNoPrinterCore(ImpressoraDTO impressoraDTO, EmpresaJson empresaJson,
 			UnidadeJson unidadeJson) throws Exception {
-<<<<<<< HEAD
+
 		ImpressoraJson json = new ImpressoraJson(soHelper.getMacAddress(), StringUtils.isEmpty(impressoraDTO.getNome()) ? impressoraDTO.getNomeRede() : impressoraDTO.getNome() , empresaJson.getCod(), unidadeJson.getMnemonico());
-=======
-		ImpressoraJson json = new ImpressoraJson(soHelper.getMacAddress(), impressoraDTO.getNome(), empresaJson.getCod(), unidadeJson.getMnemonico());
->>>>>>> 99dee9c8fae28b89d35ac48b9065ef2f2918c2d4
+
 		printerCoreService.criarDadosParaImpressao(json);
 	}
 
