f27196800d96cc86eb5e0bf3b7294dbbc2a1dc24
Adisson Gomes <adisson.marinho@dasa.com.br>
2019-04-09 19:17:05 -0300

    Imprime etiqueta 44x30mm e 30x50mm

--- src/main/java/br/com/dasa/print/core/service/PrinterService.java
+++ src/main/java/br/com/dasa/print/core/service/PrinterService.java
@@ -1,4 +1,4 @@
-package br.com.dasa.print.core.redis.service;
+package br.com.dasa.print.core.service;
 
 import javax.annotation.PostConstruct;
 import javax.print.DocFlavor;
@@ -41,7 +41,7 @@ public class PrinterService {
 	}
 
 	@Async
-	public String imprimir(Impressao impressao) {
+	public String convertToEPL2(Impressao impressao) {
 		log.info("Imprimindo");
 		log.info(impressao.getConteudoImpressao());
 		String texto = impressao.getConteudoImpressao();
@@ -50,11 +50,9 @@ public class PrinterService {
 		String[] risk = texto.split("\250");
 		StringBuilder strHexa = new StringBuilder();
 
-		String strInfoEtiqueta = buildInfoEtiqueta(impressao.getTipoEtiqueta());
-
 		for (int i = 0; i < etqCont; ++i) {
 			try {
-				strHexa.append(formataEPL2(strInfoEtiqueta, risk[i]));
+				strHexa.append(formataEPL2(impressao.getTipoEtiqueta(), risk[i]));
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
 			}
@@ -62,6 +60,23 @@ public class PrinterService {
 
 		log.debug("label result:\n{}", strHexa.toString());
 		return strHexa.toString();
+	}
+
+	/**
+	 * CHAR	DEC	OCTAL
+	 * ¾	190	276		=> caractere indicativo para quebra de linha em etiqueta
+	 * ¨	168	250		=> caractere indicativo para fim de etiqueta
+	 * @param tipoEtiqueta
+	 * @param texto
+	 * @return conteudo EPL2
+	 */
+	private String formataEPL2(TipoEtiqueta tipoEtiqueta, String texto) {
+		String header = buildInfoEtiqueta(tipoEtiqueta);
+		if (tipoEtiqueta == TipoEtiqueta.PORTRAIT) {
+			return portraitEPL(header, texto);
+		} else {
+			return landscapeEPL(header, texto);
+		}
 	}
 
 	private String buildInfoEtiqueta(TipoEtiqueta tipoEtiqueta) {
@@ -77,71 +92,64 @@ public class PrinterService {
 		return header;
 	}
 
-	/**
-	 * CHAR	DEC	OCTAL
-	 * ¾	190	276		=> caractere indicativo para quebra de linha em etiqueta
-	 * ¨	168	250		=> caractere indicativo para fim de etiqueta
-	 * @param strAbrir
-	 * @param texto
-	 * @return
-	 */
-	private String formataEPL2(String strAbrir, String texto) {
+	private String portraitEPL(String header, String texto) {
 		String[] linhas = texto.split(Character.toString((char) 190));
 		if (linhas[0].equals("Imagem")) {
 			// imprimeImagem(linhas);
 			return null;
 		} else {
-			String strHexa = "";
-
-			String strProg = new String();
-			if (linhas[0].equals("I2o5")) {
-				strProg = "2C";
-			}
-
-			if (linhas[0].equals("DAS")) {
-				strProg = "2";
-			}
-
-			if (linhas[0].equals("C128")) {
-				strProg = "1";
-			}
-
-			String strFim = "P1" + LINE_FEED;
-			strHexa = strHexa + strAbrir;
-			strHexa = strHexa + "A225,10,1,3,1,1,N," + ASPAS + linhas[1] + ASPAS + LINE_FEED;
-			strHexa = strHexa + "A200,10,1,3,1,1,N," + ASPAS + linhas[2] + ASPAS + LINE_FEED;
-			strHexa = strHexa + "A175,10,1,3,1,1,N," + ASPAS + linhas[3] + ASPAS + LINE_FEED;
-			if (linhas[0].equals("C128") && linhas[4].length() > 8) {
-				strHexa = strHexa + "A150,10,1,1,1,1,N," + ASPAS + linhas[4] + ASPAS + LINE_FEED;
-			} else {
-				strHexa = strHexa + "A150,10,1,3,1,1,N," + ASPAS + linhas[4] + ASPAS + LINE_FEED;
-			}
-
-			if (linhas[0].equals("C128")) {
-				strHexa = strHexa + "A100,10,1,3,1,1,N," + ASPAS + linhas[6] + ASPAS + LINE_FEED;
-			}
-
-			if (linhas[0].equals("I2o5") || linhas[0].equals("DAS")) {
-				if (linhas[6].contains("DL")) {
-					strHexa = strHexa + "A100,10,1,3,1,1,R," + ASPAS + linhas[6] + ASPAS + LINE_FEED;
+			String strHexa = header;
+			String strProg = getStrProg(linhas[0]);
+			int x = 225;
+			int xdelta = 25;
+			String asciiTemplate = "A%d,10,1,2,1,1,N,\"%s\"\n";
+			for(int i = 1; i < linhas.length; i++) {
+				if (i == linhas.length - 1) {
+					strHexa += "B" + x + ",10,1," + strProg + ",2,6,64,N," + ASPAS + linhas[5] + ASPAS + LINE_FEED;
 				} else {
-					strHexa = strHexa + "A100,10,1,3,1,1,N," + ASPAS + linhas[6] + ASPAS + LINE_FEED;
+					strHexa += String.format(asciiTemplate, x, linhas[i]);
 				}
+				x -= xdelta;
 			}
-
-			if (linhas[0].equals("I2o5") || linhas[0].equals("DAS")) {
-				strHexa = strHexa + "B80,90,1," + strProg + ",2,6,64,N," + ASPAS + linhas[5] + ASPAS + LINE_FEED;
-			}
-
-			if (linhas[0].equals("C128")) {
-				strHexa = strHexa + "B80,10,1," + strProg + ",2,6,64,N," + ASPAS + linhas[5] + ASPAS + LINE_FEED;
-			}
-
-			strHexa = strHexa + "A125,10,1,3,1,1,N," + ASPAS + linhas[7] + ASPAS + LINE_FEED;
-			strHexa = strHexa + strFim;
-
+			strHexa += "P1\n";
 			return strHexa;
 		}
+	}
+
+	private String landscapeEPL(String header, String texto) {
+		String[] linhas = texto.split(Character.toString((char) 190));
+		if (linhas[0].equals("Imagem")) {
+			// imprimeImagem(linhas);
+			return null;
+		} else {
+			String strHexa = header;
+			String strProg = getStrProg(linhas[0]);
+			int y = 10;
+			int ydelta = 25;
+			String asciiTemplate = "A10,%d,0,2,1,1,N,\"%s\"\n";
+			String strFim = "P1\n";
+			for (int i = 1; i < linhas.length; i++) {
+				if (i == linhas.length - 1) {
+					strHexa += "B10,155,0," + strProg + ",2,6,64,N," + ASPAS + linhas[5] + ASPAS + LINE_FEED;
+				} else {
+					strHexa += String.format(asciiTemplate, y, linhas[1]);
+				}
+				y += ydelta;
+			}
+			strHexa += strFim;
+			return strHexa;
+		}
+	}
+
+
+	private String getStrProg(String linha) {
+		if (linha.equals("DAS")) {
+			return "2";
+		}
+		if (linha.equals("C128")) {
+			return "1";
+		}
+		return "2C";
 	}
 
 	private String asciiToChar(int intValor) {
@@ -155,7 +163,7 @@ public class PrinterService {
 		Impressao impressao = new Impressao("DAP",
 				"I2o5¾        FINAL DE LOTE         ¾Testes Processos de Atendiment¾888/1184¾QTD REC: 2  LEITO: 11¾¾NUM CHAMADA: null¾COLETADOR: ¾¨I2o5¾        FINAL DE LOTE         ¾Testes Processos de Atendiment¾888/1184¾QTD REC: 2  LEITO: 11¾¾NUM CHAMADA: null¾COLETADOR: ¾¨",
 				TipoEtiqueta.PORTRAIT);
-		service.imprimir(impressao);
+		service.convertToEPL2(impressao);
 	}
 
 }
