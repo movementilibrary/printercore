6704b97f38e5d446729c121d402a7459be7eef1d
Adisson Gomes <adisson.marinho@dasa.com.br>
2019-05-04 06:28:27 -0300

    [Devops] - correcoes pipeline para deploy em Kubernetes
    Squashed commit of the following:
    
    commit aac8a6042a7dd2caabc07e75a3ed37734ce2f10d
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Sat May 4 06:08:13 2019 -0300
    
        cria spring profile de deploy
    
    commit ab08721ef2d7b099b599f35b211e0f058263f5c0
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Sat May 4 06:00:08 2019 -0300
    
        alterar envvars
    
    commit e0e763a809598dee885ac9f718f1a34ccec6faec
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Sat May 4 05:37:49 2019 -0300
    
        teste de args
    
    commit 2e7f4a9e4d68bca333a2b17980709d93fae11c2d
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Sat May 4 05:11:19 2019 -0300
    
        corrige nome projeto
    
    commit 603d91f923e7f3ed8073e2a1f5ca3fcd5d9b5eb9
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Fri May 3 17:26:31 2019 -0300
    
        correcao variavel dockerfile
    
    commit 68363f4ecadcaa4f1b901fa21d0f661365379460
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Fri May 3 17:05:24 2019 -0300
    
        correcao porta docker
    
    commit f4748da1f37f56342b6ad044fe7024e99e4919db
    Author: Adisson Gomes <adisson.marinho@dasa.com.br>
    Date:   Fri May 3 15:33:06 2019 -0300
    
        correcao nome de pacote

--- src/test/java/br/com/dasa/printcore/resource/ImpressoraResourceTest.java
+++ src/test/java/br/com/dasa/printcore/resource/ImpressoraResourceTest.java
@@ -1,27 +1,4 @@
 package br.com.dasa.printcore.resource;
-import br.com.dasa.printcore.redis.model.Impressora;
-import br.com.dasa.printcore.redis.repository.ImpressoraRepository;
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.path.json.JsonPath;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.BDDMockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-
-import static com.jayway.restassured.RestAssured.*;
-import static org.junit.Assert.assertEquals;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
